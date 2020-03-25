3dbe7dab00aec77f4083fbf4db0635997fa166cb
haoit <haoitk38@gmail.com>
2020-03-25 23:08:00 +0700

    Updates

---
+++ precache-manifest.999b2574dbd2c3c97a7fc2cfcc8533b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a4cc37d4cb65a42e0304d4ac48903b",
+    "url": "/react-sfile/index.html"
+  },
+  {
+    "revision": "797c5ae5997cbabf0219",
+    "url": "/react-sfile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e70ef1afa034467e58a0",
+    "url": "/react-sfile/static/js/2.f4a7eca6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sfile/static/js/2.f4a7eca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797c5ae5997cbabf0219",
+    "url": "/react-sfile/static/js/main.29e55382.chunk.js"
+  },
+  {
+    "revision": "aa73f756820f464dfe1f",
+    "url": "/react-sfile/static/js/runtime-main.49dd30ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sfile/static/media/logo.5d5d9eef.svg"
+  }
+]);
